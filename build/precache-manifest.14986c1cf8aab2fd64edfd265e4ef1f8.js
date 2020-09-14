d79ab26222fbb729ed8c770843d8678d6b9d0cb6
henryVentura14 <henry.ventura14@gmail.com>
2020-09-14 18:31:55 -0500

    allow build

---
+++ build/precache-manifest.14986c1cf8aab2fd64edfd265e4ef1f8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "181f4fb1b62fcb982a7576ca669459ef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7d2a84e254e1034d4b6d",
+    "url": "/static/css/main.d20d6c25.chunk.css"
+  },
+  {
+    "revision": "367958362dbf496144aa",
+    "url": "/static/js/2.739b3585.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.739b3585.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d2a84e254e1034d4b6d",
+    "url": "/static/js/main.668e8735.chunk.js"
+  },
+  {
+    "revision": "1cc7b033973417ac1e03",
+    "url": "/static/js/runtime-main.d0d758ba.js"
+  },
+  {
+    "revision": "427a4adc3c0a0dec192d14617aaa6c4c",
+    "url": "/static/media/logo.427a4adc.svg"
+  }
+]);
